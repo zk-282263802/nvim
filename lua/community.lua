5dfd3ceaf55c9f9e414abfe90862af30b10b8308
天晴😚的MacBookPro <282263802@qq.com>
2025-01-26 17:23:28 +0800

    [nevim] 1. add config files first

---
+++ lua/community.lua
@@ -0,0 +1,14 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.recipes.heirline-clock-statusline" },
+  { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
+  -- import/override with your plugins folder
+}
---
+++ lua/plugins/heirline.lua
@@ -0,0 +1,140 @@
+return {
+  "rebelot/heirline.nvim",
+  dependencies = {
+    { -- configure AstroUI to include a new UI icon
+      "AstroNvim/astroui",
+      ---@type AstroUIOpts
+      opts = {
+        icons = {
+          Clock = "", -- add icon for clock
+        },
+      },
+    },
+  },
+  opts = function(_, opts)
+    local status = require "astroui.status"
+
+    opts.statusline = { -- statusline
+      hl = { fg = "fg", bg = "bg" },
+      status.component.mode {
+        mode_text = { padding = { left = 1, right = 1 } },
+      },
+      status.component.git_branch(),
+      status.component.file_info(),
+      status.component.git_diff(),
+      status.component.diagnostics(),
+      status.component.fill(),
+      status.component.cmd_info(),
+      status.component.fill(),
+      status.component.lsp(),
+      status.component.virtual_env(),
+      status.component.treesitter(),
+      status.component.nav(),
+      -- Create a custom component to display the time
+      status.component.builder {
+        {
+          provider = function()
+            local time = os.date "%H:%M" -- show hour and minute in 24 hour format
+            ---@cast time string
+            return status.utils.stylize(time, {
+              icon = { kind = "Clock", padding = { right = 1 } }, -- use our new clock icon
+              padding = { right = 1 }, -- pad the right side so it's not cramped
+            })
+          end,
+        },
+        update = { -- update should happen when the mode has changed as well as when the time has changed
+          "User", -- We can use the User autocmd event space to tell the component when to update
+          "ModeChanged",
+          callback = vim.schedule_wrap(function(_, args)
+            if -- update on user UpdateTime event and mode change
+              (args.event == "User" and args.match == "UpdateTime")
+              or (args.event == "ModeChanged" and args.match:match ".*:.*")
+            then
+              vim.cmd.redrawstatus() -- redraw on update
+            end
+          end),
+        },
+        hl = status.hl.get_attributes "mode", -- highlight based on mode attributes
+        surround = { separator = "right", color = status.hl.mode_bg }, -- background highlight based on mode
+      },
+    }
+
+    -- Now that we have the component, we need a timer to emit the User UpdateTime event
+    vim.uv.new_timer():start( -- timer for updating the time
+      (60 - tonumber(os.date "%S")) * 1000, -- offset timer based on current seconds past the minute
+      60000, -- update every 60 seconds
+      vim.schedule_wrap(function()
+        vim.api.nvim_exec_autocmds( -- emit our new User event
+          "User",
+          { pattern = "UpdateTime", modeline = false }
+        )
+      end)
+    )
+
+    opts.winbar = { -- winbar
+      init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
+      fallthrough = false,
+      { -- inactive winbar
+        condition = function() return not status.condition.is_active() end,
+        status.component.separated_path(),
+        status.component.file_info {
+          file_icon = {
+            hl = status.hl.file_icon "winbar",
+            padding = { left = 0 },
+          },
+          filename = {},
+          filetype = false,
+          file_read_only = false,
+          hl = status.hl.get_attributes("winbarnc", true),
+          surround = false,
+          update = "BufEnter",
+        },
+      },
+      { -- active winbar
+        status.component.breadcrumbs {
+          hl = status.hl.get_attributes("winbar", true),
+        },
+      },
+    }
+
+    opts.tabline = { -- tabline
+      { -- file tree padding
+        condition = function(self)
+          self.winid = vim.api.nvim_tabpage_list_wins(0)[1]
+          self.winwidth = vim.api.nvim_win_get_width(self.winid)
+          return self.winwidth ~= vim.o.columns -- only apply to sidebars
+            and not require("astrocore.buffer").is_valid(vim.api.nvim_win_get_buf(self.winid)) -- if buffer is not in tabline
+        end,
+        provider = function(self) return (" "):rep(self.winwidth + 1) end,
+        hl = { bg = "tabline_bg" },
+      },
+      status.heirline.make_buflist(status.component.tabline_file_info()), -- component for each buffer tab
+      status.component.fill { hl = { bg = "tabline_bg" } }, -- fill the rest of the tabline with background color
+      { -- tab list
+        condition = function() return #vim.api.nvim_list_tabpages() >= 2 end, -- only show tabs if there are more than one
+        status.heirline.make_tablist { -- component for each tab
+          provider = status.provider.tabnr(),
+          hl = function(self) return status.hl.get_attributes(status.heirline.tab_type(self, "tab"), true) end,
+        },
+        { -- close button for current tab
+          provider = status.provider.close_button {
+            kind = "TabClose",
+            padding = { left = 1, right = 1 },
+          },
+          hl = status.hl.get_attributes("tab_close", true),
+          on_click = {
+            callback = function() require("astrocore.buffer").close_tab() end,
+            name = "heirline_tabline_close_tab_callback",
+          },
+        },
+      },
+    }
+
+    opts.statuscolumn = { -- statuscolumn
+      init = function(self) self.bufnr = vim.api.nvim_get_current_buf() end,
+      status.component.foldcolumn(),
+      status.component.numbercolumn(),
+      status.component.signcolumn(),
+    }
+  end,
+}
